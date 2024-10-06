954fef23a8396bf1d226b50069f14fb3e67fc849
MajeFlz <42269070+MajeFlz@users.noreply.github.com>
2024-10-06 17:08:23 +0500

    Add files via upload

---
+++ MySecondTestAppSpringBoot/src/main/java/ru/belous/MySecondTestAppSpringBoot/service/ValidationService.java
@@ -0,0 +1,11 @@
+package ru.belous.MySecondTestAppSpringBoot.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import ru.belous.MySecondTestAppSpringBoot.exception.UnsupportedCodeException;
+import ru.belous.MySecondTestAppSpringBoot.exception.ValidationFailedException;
+
+@Service
+public interface ValidationService {
+    void isValid(BindingResult bindingResult) throws ValidationFailedException, UnsupportedCodeException;;
+}
